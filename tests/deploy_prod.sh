d443b20b34d57fb476b92e1017c717fe10f88aa0
jay.jain <jay.jain@sony.com>
2021-08-15 18:45:32 -0500

    test: update deploy scripts

--- tests/deploy_prod.sh
+++ tests/deploy_prod.sh
@@ -10,18 +10,32 @@ fi
 
 CURRENT_VERSION=$(cut -d "=" -f 2 <<< $(cat setup.cfg | grep current_version) | xargs)
 echo "CURRENT VERSION is: $CURRENT_VERSION"
-# Bump Patch Version
-bumpversion --current-version $CURRENT_VERSION patch setup.py gl_query/__init__.py --allow-dirty
-NEW_VERSION=$(cut -d "=" -f 2 <<< $(cat setup.cfg | grep current_version) | xargs)
+if [[ "$CURRENT_VERSION" = "0.0.0" ]]
+then
+  echo "Setting NEW VERSION to CURRENT_VERSION ! "
+  NEW_VERSION=$CURRENT_VERSION
+else
+  # Bump Patch Version
+  bumpversion --current-version $CURRENT_VERSION patch setup.py gl_query/__init__.py --allow-dirty
+  NEW_VERSION=$(cut -d "=" -f 2 <<< $(cat setup.cfg | grep current_version) | xargs)
+fi
 echo "NEW VERSION is: $NEW_VERSION"
 
 
 # Build
 python3 setup.py sdist bdist_wheel
 
+## Create Unique filename for .whl and .tar.gz
+HASH=`openssl rand -hex 12`
+WHL_FILENAME="gl_query-${NEW_VERSION}-py3-none-any-${HASH}.whl"
+TAR_FILENAME="gl-query-${NEW_VERSION}-${HASH}.tar.gz"
+# Rename file
+mv dist/gl_query-${NEW_VERSION}-py3-none-any.whl dist/$WHL_FILENAME
+mv dist/gl-query-${NEW_VERSION}.tar.gz dist/$TAR_FILENAME
+
 # Validate
-twine check dist/gl_query-$NEW_VERSION*
-twine check dist/gl-query-$NEW_VERSION*
+twine check dist/$WHL_FILENAME
+twine check dist/$TAR_FILENAME
 
 # Deploy
 printf "Are YOU 100%% SURE you would like to deploy the new version of gl-query ( $NEW_VERSION ) to PRODUCTION?\n\
@@ -30,14 +44,18 @@ read input
 if [[ $input -eq "YES" ]] || [[ $input -eq "yes" ]] || [[ $input -eq "Yes" ]]
 then
     # You need to make sure TWINE_USERNAME and TWINE_PASSWORD env. variables are set
-    twine upload --repository-url https://test.pypi.org/legacy/ dist/gl_query-$NEW_VERSION* --non-interactive
-    twine upload --repository-url https://test.pypi.org/legacy/ dist/gl-query-$NEW_VERSION* --non-interactive
+    twine upload dist/$WHL_FILENAME --non-interactive
+    twine upload dist/$TAR_FILENAME --non-interactive
 fi
 
 pip3 uninstall gl-query -y
-until pip3 install -i https://test.pypi.org/simple/ gl-query==$NEW_VERSION --no-cache-dir
+until pip3 install gl-query==$NEW_VERSION --no-cache-dir
 do
   echo "Failed. Trying again."
 done
 
 gl-query --version
+
+## Clean up local dist
+rm dist/$WHL_FILENAME || true
+rm dist/$TAR_FILENAME || true
--- tests/deploy_test.sh
+++ tests/deploy_test.sh
@@ -8,39 +8,50 @@ fi
 
 CURRENT_VERSION=$(cut -d "=" -f 2 <<< $(cat setup.cfg | grep current_version) | xargs)
 echo "CURRENT VERSION is: $CURRENT_VERSION"
-# Bump Patch Version
-bumpversion --current-version $CURRENT_VERSION patch setup.py gl_query/__init__.py --allow-dirty
-NEW_VERSION=$(cut -d "=" -f 2 <<< $(cat setup.cfg | grep current_version) | xargs)
+if [[ "$CURRENT_VERSION" = "0.0.0" ]]
+then
+  echo "Setting NEW VERSION to CURRENT_VERSION ! "
+  NEW_VERSION=$CURRENT_VERSION
+else
+  # Bump Patch Version
+  bumpversion --current-version $CURRENT_VERSION patch setup.py gl_query/__init__.py --allow-dirty
+  NEW_VERSION=$(cut -d "=" -f 2 <<< $(cat setup.cfg | grep current_version) | xargs)
+fi
 echo "NEW VERSION is: $NEW_VERSION"
-
 
 # Build
 python3 setup.py sdist bdist_wheel
 
+## Create Unique filename for .whl and .tar.gz
+HASH=`openssl rand -hex 12 | cut -c1-5`
+WHL_FILENAME="gl_query-${NEW_VERSION}${HASH}-py3-none-any.whl"
+TAR_FILENAME="gl-query-${NEW_VERSION}-${HASH}.tar.gz"
+# Rename file
+mv dist/gl_query-${NEW_VERSION}-py3-none-any.whl dist/$WHL_FILENAME
+mv dist/gl-query-${NEW_VERSION}.tar.gz dist/$TAR_FILENAME
+
 # Validate
-twine check dist/gl_query-$NEW_VERSION*
-twine check dist/gl-query-$NEW_VERSION*
+twine check dist/$WHL_FILENAME
+twine check dist/$TAR_FILENAME
 
 # Deploy
 ### You need to make sure TWINE_USERNAME and TWINE_PASSWORD env. variables are set
-twine upload --repository-url https://test.pypi.org/legacy/ dist/gl_query-$NEW_VERSION* --non-interactive
-twine upload --repository-url https://test.pypi.org/legacy/ dist/gl-query-$NEW_VERSION* --non-interactive
-
-# sleep 8
-
-# echo "Would you like to install the new version of gl-query ( $NEW_VERSION )? ( YES or NO ) "
-# read input
-# if [[ $input -eq "YES" ]] || [[ $input -eq "yes" ]] || [[ $input -eq "Yes" ]]
-# then
-#   pip3 uninstall gl-query
-#   pip3 install -i https://test.pypi.org/simple/ gl-query==$NEW_VERSION || true
-#   pip3 install -i https://test.pypi.org/simple/ gl-query==$NEW_VERSION || true
-# fi
+twine upload --repository-url https://test.pypi.org/legacy/ dist/$WHL_FILENAME --non-interactive
+twine upload --repository-url https://test.pypi.org/legacy/ dist/$TAR_FILENAME --non-interactive
 
 pip3 uninstall gl-query -y
-until pip3 install -i https://test.pypi.org/simple/ gl-query==$NEW_VERSION --no-cache-dir
+until pip3 install -i https://test.pypi.org/simple/ gl-query==${NEW_VERSION}${HASH} --no-cache-dir
 do
   echo "Failed. Trying again."
 done
 
 gl-query --version
+
+if [[ $? -eq "0" ]]
+then
+  echo "Deployed to test.pypi.org successfully. Please make sure you have committed your changes if version numbers have been updated."
+fi
+
+## Clean up local dist
+rm dist/$WHL_FILENAME || true
+rm dist/$TAR_FILENAME || true
